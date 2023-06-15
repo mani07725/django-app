4f4c50f2bbeb521dd6cd908736408ac4982bdcb4
usman <sensaigojo113@gmail.com>
2023-06-15 23:34:46 +0500

    added requirement file for hosting

--- online_learning_platform/urls.py
+++ online_learning_platform/urls.py
@@ -3,8 +3,14 @@ from django.urls import path, include
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+from django.conf.urls import url
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('learning.urls')),
+    url(r'^media/(?P<path>.*)$', serve,
+        {'document_root':       settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,
+        {'document_root': settings.STATIC_ROOT}),
     # Other URL patterns for other apps or custom paths
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
